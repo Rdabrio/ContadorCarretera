82cd8cc28ce329615863e17320ce8532b4173eec
dabrio <ruben.dabrio@gmail.com>
2023-07-15 21:33:05 +0200

    div contador + mitad gestion de timer click

--- src/public/actualizador.js
+++ src/public/actualizador.js
@@ -1,9 +1,46 @@
 document.querySelector('button').addEventListener('click', actualizarNumero)
 window.onload = mostrarNumero
 
+var minutos = 0
+var segundos = 0
+var vecesClick = 0
+var mostrarDiv = false
 
-async function actualizarNumero() {
-    console.log('click')
+function actualizarNumero() {
+
+    if (vecesClick === 0) {
+        actualiza()
+        minutos = 5
+        segundos = 1
+        iniciaContador()
+    }
+    else if (vecesClick === 1) {
+        mostrarDiv = true
+    }
+
+    if (mostrarDiv) {
+        document.getElementById("contador").innerHTML = `0${minutos}:${segundos}`
+        document.getElementById("error-boton").style.opacity = 1;
+        document.getElementById("error-boton").classList.add("animacionMoverse")
+        mostrarDiv = false
+    }
+    ++vecesClick 
+
+    //de momento funciona con dos clicks seguidos, toca perfeccionarlo y hacerlo funcional
+}
+
+function iniciaContador() {
+    if (minutos === 0 && segundos === 0) return
+    --segundos
+    if (segundos < 0) {
+        --minutos
+        segundos = 59
+    }
+    console.log(`0${minutos}:${segundos}`)
+    setTimeout(iniciaContador, 1000)
+}
+
+async function actualiza() {
     let numero = parseInt(document.getElementById('numero').innerHTML)
     numero += 1
 
@@ -23,7 +60,6 @@ async function actualizarNumero() {
     .catch(error => {
         throw(error);
     })
-
     document.getElementById('numero').innerHTML = response.number
 }
 
