547911877e60cdca728f3151b78281b706f20ab6
dabrio <ruben.dabrio@gmail.com>
2023-07-16 00:47:25 +0200

    contador limite click global + animacion funcional

--- src/public/actualizador.js
+++ src/public/actualizador.js
@@ -4,40 +4,56 @@ window.onload = mostrarNumero
 var minutos = 0
 var segundos = 0
 var vecesClick = 0
-var mostrarDiv = false
 
 function actualizarNumero() {
 
-    if (vecesClick === 0) {
+    if (vecesClick === 0 && minutos === 0 && segundos === 0) {
         actualiza()
-        minutos = 5
-        segundos = 1
-        iniciaContador()
-    }
-    else if (vecesClick === 1) {
-        mostrarDiv = true
+        if (segundos === 0 && minutos === 0) {
+            minutos = 5
+            segundos = 1
+            localStorage.setItem('horaActual', `${new Date().toLocaleTimeString()}`)
+            iniciaContador()    
+        }
+        
     }
 
-    if (mostrarDiv) {
-        document.getElementById("contador").innerHTML = `0${minutos}:${segundos}`
-        document.getElementById("error-boton").style.opacity = 1;
-        document.getElementById("error-boton").classList.add("animacionMoverse")
-        mostrarDiv = false
+    else {
+        const errorBoton = document.getElementById("error-boton")
+        if (errorBoton.classList.contains("animacionMoverse")) {
+            errorBoton.classList.remove("animacionMoverse")
+            setTimeout(() => {
+                errorBoton.classList.add("animacionMoverse")
+            }, 100)
+        }
+        else errorBoton.classList.add("animacionMoverse")
     }
+    
     ++vecesClick 
-
-    //de momento funciona con dos clicks seguidos, toca perfeccionarlo y hacerlo funcional
 }
 
 function iniciaContador() {
-    if (minutos === 0 && segundos === 0) return
+    
+    if (minutos === 0 && segundos === 0) {
+        localStorage.setItem('segundos', `${segundos}`)
+        localStorage.setItem('minutos', `${minutos}`)
+        vecesClick = 0
+        return
+    }
     --segundos
     if (segundos < 0) {
         --minutos
         segundos = 59
     }
-    console.log(`0${minutos}:${segundos}`)
+    if (vecesClick >= 1) document.getElementById("contador").innerHTML = `0${minutos}:${formatoSegundos(segundos)}`
+    localStorage.setItem('segundos', `${segundos}`)
+    localStorage.setItem('minutos', `${minutos}`)
     setTimeout(iniciaContador, 1000)
+}
+
+function formatoSegundos(numero) {
+    if (numero >= 0 && numero < 10) return `0${numero}`
+    return numero
 }
 
 async function actualiza() {
@@ -64,6 +80,12 @@ async function actualiza() {
 }
 
 async function mostrarNumero() {
+
+    calculaTiempo()    
+    ++vecesClick
+    localStorage.setItem('horaActual', `${new Date().toLocaleTimeString()}`)
+    iniciaContador()
+
     const response = await fetch('http://localhost:3000/data')
     .then(response => response.json())
     .catch(error => {
@@ -71,4 +93,44 @@ async function mostrarNumero() {
     })
 
     document.getElementById('numero').innerHTML = response.number
+}
+
+function calculaTiempo() {
+    if (localStorage.getItem('segundos') !== null && localStorage.getItem('minutos') !== null) {
+        let segundosContador = localStorage.getItem('segundos')
+        let minutosContador = localStorage.getItem('minutos')
+
+        if (localStorage.getItem('horaActual') !== null) {
+            const horaGuardada = localStorage.getItem('horaActual').split(':')
+            const horaActual = new Date().toLocaleTimeString().split(':')
+            console.log(`horaGuardada: ${horaGuardada} horaActual: ${horaActual}`)
+
+            if (parseInt(horaActual[1]) < parseInt(horaGuardada[1])) segundos = minutos = 0
+            else {
+                let minutosPasados = parseInt(horaActual[1]) - parseInt(horaGuardada[1])
+                let segundosPasados = parseInt(horaActual[2]) - parseInt(horaGuardada[2])
+
+                if (segundosPasados < 0) {
+                    segundosPasados += 60
+                    --minutosPasados
+                }
+
+                if ((minutosPasados >= minutosContador) || (minutosPasados === minutosContador && segundosPasados >= segundosContador)) segundos = minutos = 0
+                else {
+                    minutos = minutosContador - minutosPasados
+                    segundos = segundosContador - segundosPasados
+                    if (segundos < 0) {
+                        segundos += 60
+                        --minutos
+                    }
+                }
+            }
+
+            
+        }
+        else {
+            localStorage.setItem('horaActual', `${new Date().toLocaleTimeString()}`)
+            location.reload()
+        }
+    }
 }
