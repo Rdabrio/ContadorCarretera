1f723c4dd08ddb7fd0460606c3162427ce7266b4
dabrio <ruben.dabrio@gmail.com>
2023-08-12 21:33:20 +0200

    estilo historial

--- src/public/historial.js
+++ src/public/historial.js
@@ -9,8 +9,26 @@ async function muestraDatos() {
     console.log(response)
     for (let i = response.length-1; i >= 0; --i) {
         const container = document.createElement("div")
-        container.innerHTML = `Nombre: ${response[i].nombre}  Fecha: ${response[i].fecha}`
         container.classList.add("infodiv")
         document.getElementById("infocontainer").appendChild(container)
+
+        const div = document.createElement("div")
+        div.classList.add("h3leftcontainer")
+        container.appendChild(div)
+        const h3 = document.createElement("h3")
+        h3.innerHTML = `${response[i].nombre}`  
+        div.appendChild(h3)
+
+        const div3 = document.createElement("div")
+        div3.classList.add("lineadiv")
+        container.appendChild(div3)
+
+        const div2 = document.createElement("div")
+        div2.classList.add("h3rightcontainer")
+        container.appendChild(div2)
+        const h32 = document.createElement("h3")
+        h32.innerHTML = `${response[i].fecha}`
+        div2.appendChild(h32)
+        
     }
 }
